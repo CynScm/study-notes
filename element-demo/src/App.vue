940aa51629dd0cf00c254799c43f1d7bdb4d4594
cyn <1909178414@qq.com>
2018-05-24 22:38:59 +0800

    add element scss

--- element-demo/src/App.vue
+++ element-demo/src/App.vue
@@ -30,7 +30,9 @@ export default {
 }
 </script>
 
-<style>
+
+
+<style lang="scss" >
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -38,8 +40,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
 h1, h2 {
   font-weight: normal;
 }
@@ -57,4 +57,6 @@ li {
 a {
   color: #42b983;
 }
+}
+
 </style>
